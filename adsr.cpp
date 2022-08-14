12f809a64d85c9643814ab9a248165967a7a00ad
Mo_thunderz <76265418+mo-thunderz@users.noreply.github.com>
2022-08-14 11:49:10 +0200

    Add files via upload

--- adsr.cpp
+++ adsr.cpp
@@ -1,8 +1,8 @@
 //----------------------------------//
 // ADSR class for Arduino
 // by mo-thunderz
-// version 1.1
-// last update: 29.12.2020
+// version 1.2
+// last update: 14.08.2020
 //----------------------------------//
 
 #include "Arduino.h"
@@ -11,87 +11,95 @@
 
 adsr::adsr(int l_vertical_resolution)
 {
-	_vertical_resolution = l_vertical_resolution;						// store vertical resolution (DAC_Size)
-	_attack = 100000;																				// take 100ms as initial value for Attack
-	_sustain = l_vertical_resolution/2;											// take half the DAC_size as initial value for sustain
-	_decay = 100000;																				// take 100ms as initial value for Decay
-	_release = 100000;																			// take 100ms as initial value for Release
+    _vertical_resolution = l_vertical_resolution;                               // store vertical resolution (DAC_Size)
+    _attack = 100000;                                                           // take 100ms as initial value for Attack
+    _sustain = l_vertical_resolution/2;                                         // take half the DAC_size as initial value for sustain
+    _decay = 100000;                                                            // take 100ms as initial value for Decay
+    _release = 100000;                                                          // take 100ms as initial value for Release
 
-	for (int i = 0; i<ARRAY_SIZE; i++) {												// Create look-up table for Attack
-		_attack_table[i] = i;
-		_decay_release_table[i] = _vertical_resolution - 1 - i;
-	}
+    for (int i = 0; i<ARRAY_SIZE; i++) {                                        // Create look-up table for Attack
+        _attack_table[i] = i;
+        _decay_release_table[i] = _vertical_resolution - 1 - i;
+    }
 
-	for (int i = 0; i<ARRAY_SIZE-1; i++) {											// Create look-up table for Decay
-		_attack_table[i+1] = (1.0 - ATTACK_ALPHA) * (_vertical_resolution - 1) + ATTACK_ALPHA * _attack_table[i];
-		_decay_release_table[i+1] = ATTACK_DECAY_RELEASE * _decay_release_table[i];
-	}
+    for (int i = 0; i<ARRAY_SIZE-1; i++) {                                      // Create look-up table for Decay
+        _attack_table[i+1] = (1.0 - ATTACK_ALPHA) * (_vertical_resolution - 1) + ATTACK_ALPHA * _attack_table[i];
+        _decay_release_table[i+1] = ATTACK_DECAY_RELEASE * _decay_release_table[i];
+    }
 
-	for (int i = 0; i<ARRAY_SIZE; i++) {												// normalize table to min and max
-		_attack_table[i] = map(_attack_table[i], 0, _attack_table[ARRAY_SIZE-1], 0, _vertical_resolution - 1);
-		_decay_release_table[i] = map(_decay_release_table[i], _decay_release_table[ARRAY_SIZE-1], _decay_release_table[0], 0, _vertical_resolution - 1);
-	}
+    for (int i = 0; i<ARRAY_SIZE; i++) {                                        // normalize table to min and max
+        _attack_table[i] = map(_attack_table[i], 0, _attack_table[ARRAY_SIZE-1], 0, _vertical_resolution - 1);
+        _decay_release_table[i] = map(_decay_release_table[i], _decay_release_table[ARRAY_SIZE-1], _decay_release_table[0], 0, _vertical_resolution - 1);
+    }
+}
+
+void adsr::setResetAttack(bool l_reset_attack)
+{
+    _reset_attack = l_reset_attack;
 }
 
 void adsr::setAttack(unsigned long l_attack)
 {
-	_attack = l_attack;
+    _attack = l_attack;
 }
 
 void adsr::setDecay(unsigned long l_decay)
 {
-	_decay = l_decay;
+    _decay = l_decay;
 }
 
 void adsr::setSustain(int l_sustain)
 {
-	if(l_sustain < 0)
-		l_sustain = 0;
-	if(l_sustain >= _vertical_resolution)
-		l_sustain = _vertical_resolution - 1;
-	_sustain = l_sustain;
+    if(l_sustain < 0)
+        l_sustain = 0;
+    if(l_sustain >= _vertical_resolution)
+        l_sustain = _vertical_resolution - 1;
+    _sustain = l_sustain;
 }
 
 void adsr::setRelease(unsigned long l_release)
 {
-	_release = l_release;
+    _release = l_release;
 }
 
 void adsr::noteOn(unsigned long l_micros) {
-	_t_note_on = l_micros;							// set new timestamp for note_on
-	_attack_start = _adsr_output;				// set start value new Attack
-	_notes_pressed++;										// increase number of pressed notes with one
+    _t_note_on = l_micros;                          // set new timestamp for note_on
+    if(_reset_attack)                               // set start value new Attack
+        _attack_start = 0;							// if _reset_attack equals true, a new trigger starts with 0
+    else
+        _attack_start = _adsr_output;               // if _reset_attack equals false, a new trigger starts with the current value
+    _notes_pressed++;                               // increase number of pressed notes with one
 }
 
 void adsr::noteOff(unsigned long l_micros) {
-	_notes_pressed--;
-	if(_notes_pressed <= 0) {						// if all notes are depressed - start release
-		_t_note_off = l_micros;						// set timestamp for note off
-		_release_start = _adsr_output;		// set start value for release
-		_notes_pressed = 0;
-	}
+    _notes_pressed--;
+    if(_notes_pressed <= 0) {                       // if all notes are depressed - start release
+        _t_note_off = l_micros;                     // set timestamp for note off
+        _release_start = _adsr_output;              // set start value for release
+        _notes_pressed = 0;
+    }
 }
 
 int adsr::getWave(unsigned long l_micros)
 {
-	unsigned long delta = 0;
-	if(_t_note_off < _t_note_on)		{			// if note is pressed
-		delta = l_micros - _t_note_on;
-		if(delta < _attack)									// Attack
-			_adsr_output = map(_attack_table[(int)floor(ARRAY_SIZE * (float) delta / (float)_attack)], 0, _vertical_resolution - 1, _attack_start, _vertical_resolution - 1);		//
-		else if(delta < _attack + _decay) { // Decay
-			delta = l_micros - _t_note_on - _attack;
-			_adsr_output = map(_decay_release_table[(int)floor(ARRAY_SIZE * (float) delta / (float) _decay)], 0, _vertical_resolution - 1, _sustain, _vertical_resolution - 1);
-		}
-		else
-			_adsr_output = _sustain;
-	}
-	if(_t_note_off > _t_note_on)		{			// if note not pressed
-		delta = l_micros - _t_note_off;
-		if(delta < _release)								// release
-			_adsr_output = map(_decay_release_table[(int)floor(ARRAY_SIZE * (float) delta / (float) _release)], 0, _vertical_resolution - 1, 0, _release_start);
-		else
-			_adsr_output = 0;									// note off
-	}
-	return _adsr_output;
+    unsigned long delta = 0;
+    if(_t_note_off < _t_note_on)        {           // if note is pressed
+        delta = l_micros - _t_note_on;
+        if(delta < _attack)                         // Attack
+            _adsr_output = map(_attack_table[(int)floor(ARRAY_SIZE * (float) delta / (float)_attack)], 0, _vertical_resolution - 1, _attack_start, _vertical_resolution - 1);       //
+        else if(delta < _attack + _decay) {         // Decay
+            delta = l_micros - _t_note_on - _attack;
+            _adsr_output = map(_decay_release_table[(int)floor(ARRAY_SIZE * (float) delta / (float) _decay)], 0, _vertical_resolution - 1, _sustain, _vertical_resolution - 1);
+        }
+        else
+            _adsr_output = _sustain;
+    }
+    if(_t_note_off > _t_note_on)        {           // if note not pressed
+        delta = l_micros - _t_note_off;
+        if(delta < _release)                        // release
+            _adsr_output = map(_decay_release_table[(int)floor(ARRAY_SIZE * (float) delta / (float) _release)], 0, _vertical_resolution - 1, 0, _release_start);
+        else
+            _adsr_output = 0;                       // note off
+    }
+    return _adsr_output;
 }
--- adsr.h
+++ adsr.h
@@ -1,8 +1,8 @@
 //----------------------------------//
 // ADSR class for Arduino
 // by mo-thunderz
-// version 1.1
-// last update: 29.12.2020
+// version 1.2
+// last update: 14.08.2022
 //----------------------------------//
 
 #include "Arduino.h"
@@ -11,45 +11,47 @@
 #define ADSR
 
 // for array for lookup table
-#define ARRAY_SIZE 1024					// number of time points
-#define ATTACK_ALPHA 0.995			// varies between 0.9 (steep curve) and 0.9995 (straight line)
-#define ATTACK_DECAY_RELEASE 0.997			// fits to ARRAY_SIZE 1024
+#define ARRAY_SIZE 1024                     // number of time points
+#define ATTACK_ALPHA 0.995                  // varies between 0.9 (steep curve) and 0.9995 (straight line)
+#define ATTACK_DECAY_RELEASE 0.997          // fits to ARRAY_SIZE 1024
 
 
 // Midi trigger -> on/off
 class adsr
 {
-	public:
-		// constructor
-		adsr(int dacSize);
+    public:
+        // constructor
+        adsr(int dacSize);
 
-		void setAttack(unsigned long l_attack);
-		void setDecay(unsigned long l_decay);
-		void setSustain(int l_sustain);
-		void setRelease(unsigned long l_release);
-		void noteOn(unsigned long l_micros);
-		void noteOff(unsigned long l_micros);
-		int getWave(unsigned long l_micros);
+        void setAttack(unsigned long l_attack);
+        void setDecay(unsigned long l_decay);
+        void setSustain(int l_sustain);
+        void setRelease(unsigned long l_release);
+        void noteOn(unsigned long l_micros);
+        void noteOff(unsigned long l_micros);
+        int getWave(unsigned long l_micros);
+        void setResetAttack(bool l_reset_attack);
 
-	private:
-		int _attack_table[ARRAY_SIZE];
-		int _decay_release_table[ARRAY_SIZE];
+    private:
+        int _attack_table[ARRAY_SIZE];
+        int _decay_release_table[ARRAY_SIZE];
 
-		int _vertical_resolution;				// number of bits for output, control, etc
-		unsigned long _attack = 0;								// 0 to 20 sec
-		unsigned long _decay = 0;									// 1ms to 60 sec
-		int _sustain = 0;								// 0 to -60dB -> then -inf
-		unsigned long _release = 0;								// 1ms to 60 sec
+        int _vertical_resolution;                               // number of bits for output, control, etc
+        unsigned long _attack = 0;                              // 0 to 20 sec
+        unsigned long _decay = 0;                               // 1ms to 60 sec
+        int _sustain = 0;                                       // 0 to -60dB -> then -inf
+        unsigned long _release = 0;                             // 1ms to 60 sec
+        bool _reset_attack = false;                             // if _reset_attack is "true" a new trigger starts with 0, if _reset_attack is false it starts with the current output value
 
-		// time stamp for note on and note off
-		unsigned long _t_note_on = 0;
-		unsigned long _t_note_off = 0;
+        // time stamp for note on and note off
+        unsigned long _t_note_on = 0;
+        unsigned long _t_note_off = 0;
 
-		// internal values needed to transition to new pulse (attack) and to release at any point in time
-		int _adsr_output;
-		int _release_start;
-		int _attack_start;
-		int _notes_pressed = 0;
+        // internal values needed to transition to new pulse (attack) and to release at any point in time
+        int _adsr_output;
+        int _release_start;
+        int _attack_start;
+        int _notes_pressed = 0;
 };
 
 #endif
